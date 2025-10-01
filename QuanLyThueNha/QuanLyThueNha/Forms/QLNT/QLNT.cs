2c06c25352510d0f6a9f37e49c45e2a99d15ed02
lamngocnhuy <146564195+lamngocnhuy@users.noreply.github.com>
2025-10-02 01:32:46 +0700

    frm: mainmenu,qlts,qlnt

---
+++ QuanLyThueNha/QuanLyThueNha/Forms/QLNT/QLNT.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyThueNha.Forms
+{
+    public partial class QLNT : Form
+    {
+        public QLNT()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn__Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
